209eb2eff9c6dafd8a1cda8c07a107ad9c927959
cobalt88 <vincentteune@gmail.com>
2022-06-23 11:18:43 -0400

    6.23.22
    add in bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
+const webpack = require('webpack');
 
 
 module.exports = {
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
   mode: 'development',
   entry: "./assets/js/script.js",
   output: {
